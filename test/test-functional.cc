74ae5129257e405b875ed8e97ce882fba9be7c29
Fedor Indutny <fedor.indutny@gmail.com>
2012-02-23 04:21:20 +0600

    test: cleanup test

--- test/test-functional.cc
+++ test/test-functional.cc
@@ -7,39 +7,39 @@ TEST_START("functional test")
     assert(HNumber::Cast(result)->value() == 1);
   })
 
-  FUN_TEST("a = 32\r\nreturn a", {
+  FUN_TEST("a = 32\nreturn a", {
     assert(result != NULL);
     assert(HNumber::Cast(result)->value() == 32);
   })
 
-  FUN_TEST("a = b = 32\r\nreturn a", {
+  FUN_TEST("a = b = 32\nreturn a", {
     assert(result != NULL);
     assert(HNumber::Cast(result)->value() == 32);
   })
 
-  FUN_TEST("a = 32\r\nb = a\r\nreturn b", {
+  FUN_TEST("a = 32\nb = a\nreturn b", {
     assert(result != NULL);
     assert(HNumber::Cast(result)->value() == 32);
   })
 
   // Functions
-  FUN_TEST("a() {}\r\nreturn a", {
+  FUN_TEST("a() {}\nreturn a", {
     assert(result != NULL);
     assert(HFunction::Cast(result)->addr() != NULL);
   })
 
-  FUN_TEST("a() { return 1 }\r\nreturn a()", {
+  FUN_TEST("a() { return 1 }\nreturn a()", {
     assert(result != NULL);
     assert(HNumber::Cast(result)->value() == 1);
   })
 
   // Context slots
-  FUN_TEST("b = 13589\r\na() { scope b }\r\nreturn b", {
+  FUN_TEST("b = 13589\na() { scope b }\nreturn b", {
     assert(result != NULL);
     assert(HNumber::Cast(result)->value() == 13589);
   })
 
-  FUN_TEST("b = 13589\r\na() { scope b\r\nb = 1234 }\r\na()\r\nreturn b", {
+  FUN_TEST("a() { scope a, b\nb = 1234 }\nb = 13589\na()\nreturn b", {
     assert(result != NULL);
     assert(HNumber::Cast(result)->value() == 1234);
   })
