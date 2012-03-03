d96e9e73f84ae4382df45f775cd744cc10a55350
Fedor Indutny <fedor.indutny@gmail.com>
2012-03-03 23:26:26 +0600

    gc: last one compiler warning

--- src/gc.cc
+++ src/gc.cc
@@ -27,7 +27,7 @@ void GC::CollectGarbage(char* stack_top) {
     char* value = *slot;
 
     // Skip NULL pointers, non-pointer values and rbp pushes
-    if (value == NULL || value > top && value <= *heap()->root_stack()) {
+    if (value == NULL || (value > top && value <= *heap()->root_stack())) {
       continue;
     }
 
